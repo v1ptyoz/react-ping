69fb28a777cf4d03ccfd15b7fc46fc80ffd5e2f1
v1ptyoz <v1ptyoz@gmail.com>
2021-02-12 09:12:47 +1000

    info-layer.js'

--- src/components/info-layer/info-layer.js
+++ src/components/info-layer/info-layer.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
+import {Col} from 'reactstrap'
 
 
 export default class infoLayer extends Component {
-    
+    render() {
+        return (
+            <Col className="text-center mt-5">
+                <h3>Information:</h3>
+                <h4>Errors: 0</h4>
+                <h6>Total Devices: 3</h6>
+            </Col>
+        );
+    }
 }
