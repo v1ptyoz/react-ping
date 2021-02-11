51be232a7a0edcf7defc7c4de897a63111c1464e
v1ptyoz <v1ptyoz@gmail.com>
2021-02-11 18:28:26 +1000

    AppHeader component

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import {Container, Row, Col} from 'reactstrap'
 
 
 export default class AppHeader extends Component {
-    
+    render() {
+        return (
+            <Container className="mt-4">
+                <Row>
+                    <Col className="text-center">
+                        <h1>Ping monitoring!</h1>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import nextId from 'react-id-generator';
+import AppHeader from '../app-header'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        data : [ // mock data
+            {name: 'Google', ip: '8.8.8.8', id: nextId()},
+            {name: 'Yandex', ip: '87.250.250.242', id: nextId()},
+            {name: 'Bad IP', ip: '1', id: nextId()}
+        ],
+    };
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <AppHeader/>
     )
-  }
+  };
+
+
 }
