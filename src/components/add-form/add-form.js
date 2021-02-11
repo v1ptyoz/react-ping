21c44ed1dc16b6c993e7374ed5384916efae842c
v1ptyoz <v1ptyoz@gmail.com>
2021-02-12 09:44:02 +1000

    creating add-form component

--- src/components/add-form/add-form.js
+++ src/components/add-form/add-form.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react';
+import {Col, Button, Form, FormGroup, Label, Input} from 'reactstrap'
 
 
 export default class AddForm extends Component {
-    
+    render() {
+        return (
+            <Col xs='3' className="text-center">
+                <Form action="#">
+                    <FormGroup>
+                        <Label for="name">Name</Label>
+                        <Input className="form-control" id="name" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="ip">IP-adress</Label>
+                        <Input className="form-control" id="ip" />
+                    </FormGroup>
+                      <Button type="submit" className="btn-success">Add</Button>
+                </Form>
+            </Col>
+        )
+    } 
 }
