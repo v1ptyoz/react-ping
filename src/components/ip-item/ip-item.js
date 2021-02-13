8b006e633cfa4355f64d25886cd747285ae3724c
v1ptyoz <v1ptyoz@gmail.com>
2021-02-13 15:17:53 +1000

    rewrite code

--- src/components/ip-item/ip-item.js
+++ src/components/ip-item/ip-item.js
@@ -4,18 +4,25 @@ import {Row, Col, Button, ButtonGroup} from 'reactstrap'
 
 export default class IpItem extends Component {
     render() {
+        const {id, name, ip, onDelete} = this.props;
 
-        const {name, ip} = this.props;
+        let classNames = 'text-center pt-1';
+        
 
         return (
-            <Row className="mb-1">
-                <Col className="text-center alert-success">{name}</Col>
-                <Col className="text-center alert-success">{ip}</Col>
+            <Row key={id} className="mb-1">
+                <Col className={classNames + " alert-success"}>{name}</Col>
+                <Col className={classNames + " alert-success"}>{ip}</Col>
                 <ButtonGroup>
-                    <Button type="button" className="btn-primary"><i className="far fa-stop-circle"></i></Button>
-                    <Button type="button" className="btn-secondary"><i className="fas fa-ellipsis-v"></i></Button>
+                    <Button className="btn-primary"><i className="far fa-stop-circle"></i></Button>
+                    <Button className="btn-secondary"><i className="fas fa-ellipsis-v"></i></Button>
                 </ButtonGroup>
-                <Button className="btn-danger ml-4"><i className="fas fa-trash"></i></Button>
+                <Button 
+                    className="btn-danger ml-4"
+                    onClick={onDelete}
+                    >
+                        <i className="fas fa-trash"></i>
+                </Button>
             </Row>
         )
     }
