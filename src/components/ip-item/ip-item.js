d6c468a98a7616ac308466e75af0bad4d1dc4c48
v1ptyoz <v1ptyoz@gmail.com>
2021-02-13 11:34:29 +1000

    use variables in Cols

--- src/components/ip-item/ip-item.js
+++ src/components/ip-item/ip-item.js
@@ -9,8 +9,8 @@ export default class IpItem extends Component {
 
         return (
             <Row className="mb-1">
-                <Col className="text-center alert-success">Name</Col>
-                <Col className="text-center alert-success">IP</Col>
+                <Col className="text-center alert-success">{name}</Col>
+                <Col className="text-center alert-success">{ip}</Col>
                 <ButtonGroup>
                     <Button type="button" className="btn-primary"><i className="far fa-stop-circle"></i></Button>
                     <Button type="button" className="btn-secondary"><i className="fas fa-ellipsis-v"></i></Button>
