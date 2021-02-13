22daa48f6d6d6301d3a00a3ccafd8de06b64c04a
v1ptyoz <v1ptyoz@gmail.com>
2021-02-13 15:17:29 +1000

    ip-list

--- src/components/ip-list/ip-list.js
+++ src/components/ip-list/ip-list.js
@@ -1,6 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Row, Col, Container} from 'reactstrap'
+import IpItem from '../ip-item';
 
+const IpList = ({ipList, onDelete}) => {
+    const elements = ipList.map((item) => {
+        const {id} = item;
+        return (
+            <IpItem 
+                {...item}
+                onDelete={() => onDelete(id)}
+                key = {id}
+            />
+        )      
+});
 
-export default class IpList extends Component {
-    
+    return (
+        <Container>
+            {elements}
+        </Container>
+    )
 }
+
+export default IpList;
