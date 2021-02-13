586393fee35969cccd4d81205471ce79ba789f64
v1ptyoz <v1ptyoz@gmail.com>
2021-02-13 15:17:17 +1000

    use variable

--- src/components/info-layer/info-layer.js
+++ src/components/info-layer/info-layer.js
@@ -8,7 +8,7 @@ export default class infoLayer extends Component {
             <Col className="text-center mt-5">
                 <h3>Information:</h3>
                 <h4>Errors: 0</h4>
-                <h6>Total Devices: 3</h6>
+                <h6>Total Devices: {this.props.counter}</h6>
             </Col>
         );
     }
