7f80069f07a47d4edc207dbb2ee7ac6840cf6562
v1ptyoz <v1ptyoz@gmail.com>
2021-02-15 17:33:57 +1000

    added onStart

--- src/components/ip-list/ip-list.js
+++ src/components/ip-list/ip-list.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Row, Col, Container} from 'reactstrap'
 import IpItem from '../ip-item';
 
-const IpList = ({ipList, onDelete}) => {
+const IpList = ({ipList, onDelete, onStart}) => {
     const elements = ipList.map((item) => {
         const {id} = item;
         return (
             <IpItem 
                 {...item}
                 onDelete={() => onDelete(id)}
+                onStart={() => onStart(id)}
                 key = {id}
             />
         )      
