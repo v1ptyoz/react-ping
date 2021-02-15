c7bb1792a0c5999f88afb0144505eed550b4aae3
v1ptyoz <v1ptyoz@gmail.com>
2021-02-15 17:35:00 +1000

    coded status when start and stop button pressed

--- src/components/ip-item/ip-item.js
+++ src/components/ip-item/ip-item.js
@@ -4,17 +4,39 @@ import {Row, Col, Button, ButtonGroup} from 'reactstrap'
 
 export default class IpItem extends Component {
     render() {
-        const {id, name, ip, onDelete} = this.props;
+        const {id, name, ip, status, started, onDelete, onStart} = this.props;
 
         let classNames = 'text-center pt-1';
+        let icon = 'far';
+        let btnColor = '';
+
+
+        if (started) {
+            icon += ' fa-stop-circle';
+            btnColor += ' btn-danger';
+        } else {
+            icon += ' fa-play-circle';
+            btnColor += ' btn-info';
+        }
+
+        if (status) {
+            classNames += ' alert-success';
+        } else {
+            classNames += ' alert-danger'
+        }
         
 
         return (
             <Row key={id} className="mb-1">
-                <Col className={classNames + " alert-success"}>{name}</Col>
-                <Col className={classNames + " alert-success"}>{ip}</Col>
+                <Col className={classNames}>{name}</Col>
+                <Col className={classNames}>{ip}</Col>
                 <ButtonGroup>
-                    <Button className="btn-primary"><i className="far fa-stop-circle"></i></Button>
+                    <Button 
+                        className={btnColor}
+                        onClick={onStart}
+                    >
+                            <i className={icon}></i>
+                    </Button>
                     <Button className="btn-secondary"><i className="fas fa-ellipsis-v"></i></Button>
                 </ButtonGroup>
                 <Button 
